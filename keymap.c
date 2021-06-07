2d8bf6ee4dcf59c807947e805160533c92674d50
Ruslanas Gžibovskis <lpic.lt@lpic.lt>
2021-06-07 09:58:54 +0200

    Update keymap.c
    Swap LOWER vs RAISE ;)

--- keymap.c
+++ keymap.c
@@ -41,7 +41,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC, \
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSLS, KC_ENT, \
   RSFT_T(KC_DEL), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_SLSH), \
-  KC_LCTL, KC_LGUI, KC_LALT, RAISE,   LOWER,   KC_SPC,           KC_RALT, KC_SCLN, KC_LEFT, KC_DOWN, KC_RGHT \
+  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   RAISE,   KC_SPC,           KC_RALT, KC_SCLN, KC_LEFT, KC_DOWN, KC_RGHT \
 ),
 
 /* Raise
