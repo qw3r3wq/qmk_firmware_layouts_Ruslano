e165d5ad53645b7973f1c84b60637f145016a5a2
Ruslanas Gžibovskis <lpic.lt@lpic.lt>
2022-06-11 13:28:01 +0300

    Preonic keymap.c - Raise fixed

--- keyboards/preonic/keymaps/Ruslano/keymap.c
+++ keyboards/preonic/keymaps/Ruslano/keymap.c
@@ -46,9 +46,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Esc  |   Q  |   W  |   E  |   R  |   T  |   Y  |   U  |   I  |   O  |   P  |Bckspc|
  * |------+------+------+------+------+-------------+------+------+------+------+------|
- * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |   ;  |Enter |
+ * | Tab  |   A  |   S  |   D  |   F  |   G  |   H  |   J  |   K  |   L  |  \|  |Enter |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Shift |
+ * | Shift|   Z  |   X  |   C  |   V  |   B  |   N  |   M  |   ,  |   .  |  Up  |Shift/|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
  * | Ctrl | Alt  | GUI  |Lower |Raise |Space |Space |NUMPAD|  :;  | Left | Down |Right |
  * `-----------------------------------------------------------------------------------'
@@ -57,8 +57,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSPC,
   KC_ESC,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_BSLS, KC_ENT,
-  RSFT_T(KC_DEL), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_SLSH),
-  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   RAISE,   KC_SPC,           KC_RALT, KC_SCLN, KC_LEFT, KC_DOWN, KC_RGHT
+  RSFT_T(KC_DEL), KC_Z, KC_X, KC_C,   KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_UP,   RSFT_T(KC_SLSH),
+  KC_LCTL, KC_LGUI, KC_LALT, LOWER,   RAISE,   KC_SPC,  KC_SPC,  KC_RALT, KC_SCLN, KC_LEFT, KC_DOWN, KC_RGHT
 ),
 
 /* Raise
@@ -69,9 +69,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |------+------+------+------+------+-------------+------+------+------+------+------|
  * |   ~  |   !  |   @  |   #  |   $  |   %  |  ^   |  &   |  *   |  (   |  )   | Ent  |
  * |------+------+------+------+------+------|------+------+------+------+------+------|
- * |      |  F7  |  F8  |  F9  |  F10 |  F11 |  F12 |ISO ~ |ISO | |  [   |  ]  |RShift|
+ * |LShift|   /  |   \  |   '  |   ?  |   -  |  _   |  +   |  =   |  {   |  }   |RShift|
  * |------+------+------+------+------+------+------+------+------+------+------+------|
- * |      |      |      |      |      |             |      | Next | Vol- | Vol+ | Play |
+ * | LCTL | LGUI | LALT |PrScrn|Raise |  SP  |  SP  | RAlt | RGUI |  [   |  ]   | RCTL |
  * `-----------------------------------------------------------------------------------'
  */
 [_RAISE] = LAYOUT_preonic_grid(
