df7bef46b05f209ce01e439bb2c90398fa371c1f
Ruslanas Gzibovskis <ruslanas.g@gmail.com>
2023-09-26 12:50:12 +0300

    xd75, removed LT

--- keyboards/xiudi/xd75/keymaps/Ruslano/keymap.c
+++ keyboards/xiudi/xd75/keymaps/Ruslano/keymap.c
@@ -15,6 +15,10 @@
  */
 
 #include QMK_KEYBOARD_H
+/*
+#include "send_string.h"
+#include "keymap_lithuanian_qwerty.h"
+ */
 
 #define DEFAULT TO(_QWERTY)
 #define FUNCT MO(_FUNCT)
@@ -51,6 +55,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	KC_LCTL,	KC_LGUI,LALT_T(KC_MINS),BOT,	TOP,	KC_SPC,	KC_LEFT,KC_DOWN,KC_RGHT,KC_ENT,	NUMPAD,	KC_BSLS,KC_LEFT,KC_DOWN,KC_RGHT
     ),
     [_TOP] = LAYOUT_ortho_5x15( /* TOP NUMBER AND MISSING CHAR */
+        /*KC_ESC,		LT_AOGO,LT_CCAR,	LT_EOGO,LT_EDOT,LT_IOGO,KC_MINS,KC_GRV,	KC_EQL,	LT_SCAR,LT_UOGO,LT_UMAC,KC_9,	LT_ZCAR,KC_BSPC,*/
         KC_ESC,		KC_1,	KC_2,		KC_3,	KC_4,	KC_5,	KC_MINS,KC_GRV,	KC_EQL,	KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_BSPC,
         KC_GRV,		KC_1,	KC_2,		KC_3,	KC_4,	KC_5,	KC_MINS,KC_GRV,	KC_EQL,	KC_6,	KC_7,	KC_8,	KC_9,	KC_0,	KC_BSPC,
         KC_TILD,	KC_EXLM,KC_AT,		KC_HASH,KC_DLR,	KC_PERC,KC_HOME,KC_INS,	KC_PGUP,KC_CIRC,KC_AMPR,KC_ASTR,KC_LPRN,KC_RPRN,KC_QUOT,
@@ -79,7 +84,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
      * '--------------------------------------------------------------------------------------------------------------------------------------'
      */
     [_FUNCT] = LAYOUT_ortho_5x15( /* FUNCTION */
-        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NUM,KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
+        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_NUM,  KC_SLSH, KC_ASTR, KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,
         KC_MSEL, KC_CALC, KC_MYCM, KC_MAIL, RGB_HUD, RGB_HUI, KC_P7,   KC_P8,   KC_P9,   KC_MINS, _______, _______, KC_PSCR, KC_LSCR, KC_PAUS,
         KC_MPRV, KC_MPLY, KC_MNXT, KC_MSTP, RGB_SAD, RGB_SAI, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, _______, QK_BOOT, _______, _______, _______,
         KC_VOLD, KC_MUTE, KC_VOLU, KC_APP,  RGB_VAD, RGB_VAI, KC_P1,   KC_P2,   KC_P3,   KC_PENT, _______, QK_RBT,  _______, _______, KC_RSFT,
